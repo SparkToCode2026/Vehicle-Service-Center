bc49c894f7c26ebdfd14587158f4c728998dcd72
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-08-04 18:02:35 +0400

    add

--- Vehicle Service Center/VehicleServiceCenter/Models/Appointment.cs
+++ Vehicle Service Center/VehicleServiceCenter/Models/Appointment.cs
@@ -3,4 +3,13 @@ namespace VehicleServiceCenter.Models;
 public class Appointment
 {
     
+    public int AppointmentId { get; set; }
+
+    public DateTime AppointmentDate { get; set; }
+
+    public string Status { get; set; }
+    // Pending, Confirmed, Completed, Cancelled
+
+    public string Description { get; set; }
+
 }
