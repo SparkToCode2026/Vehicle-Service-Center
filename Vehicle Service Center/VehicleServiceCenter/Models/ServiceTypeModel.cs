a42c172f569589623e04b592c068c965dc0e9251
Marya <maryaalmaskari8@gmail.com>
2026-08-04 19:08:42 +0400

    Add service type attributes

--- Vehicle Service Center/VehicleServiceCenter/Models/ServiceTypeModel.cs
+++ Vehicle Service Center/VehicleServiceCenter/Models/ServiceTypeModel.cs
@@ -2,5 +2,10 @@
 {
     public class ServiceTypeModel
     {
+        public int ServiceTypeId { get; set; }
+        public string SName { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Model { get; set; } = string.Empty;
+        public double SPrice { get; set; }
     }
 }
