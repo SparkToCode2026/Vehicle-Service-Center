145cb77b9c8f564ae5afc67766df73f3f566102b
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-08-04 18:01:43 +0400

    Add attribute into SparePart class

--- Vehicle Service Center/VehicleServiceCenter/Models/SparePart.cs
+++ Vehicle Service Center/VehicleServiceCenter/Models/SparePart.cs
@@ -3,4 +3,17 @@ namespace VehicleServiceCenter.Models;
 public class SparePart
 {
     
+    public int SparePartId { get; set; }
+
+    public string PartName { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string Supplier { get; set; }
+
+
+    public bool IsAvailable { get; set; }
+    
 }
