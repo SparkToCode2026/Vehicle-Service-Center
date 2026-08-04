e016ba116e43e984ccb90ec1bd6d7938b3479f24
Marya <maryaalmaskari8@gmail.com>
2026-08-04 19:04:49 +0400

    Add Vehicle attributes

--- Vehicle Service Center/VehicleServiceCenter/Models/VehicleModel.cs
+++ Vehicle Service Center/VehicleServiceCenter/Models/VehicleModel.cs
@@ -2,5 +2,9 @@
 {
     public class VehicleModel
     {
+        public int VehicleId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
     }
 }
