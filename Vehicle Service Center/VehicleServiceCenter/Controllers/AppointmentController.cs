19cca6782f21545152476d93ec00c2ea8e72e5ce
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-08-05 23:54:14 +0400

    Add appointment Controler

--- Vehicle Service Center/VehicleServiceCenter/Controllers/AppointmentController.cs
+++ Vehicle Service Center/VehicleServiceCenter/Controllers/AppointmentController.cs
@@ -1,6 +1,18 @@
-namespace VehicleServiceCenter.Controllers;
 
-public class AppointmentController
+namespace VehicleServiceCenter.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using VehicleServiceCenter.Models;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AppointmentController : Controller
 {
+    private readonly ProjectContext _context;
+
+    public AppointmentController(ProjectContext context)
+    {
+        _context = context;
+    }
     
 }
