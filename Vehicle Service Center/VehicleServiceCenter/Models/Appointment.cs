f5002d44bb68e9f73497d0e7f2f63f3915184bb4
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-08-05 13:56:46 +0400

    Update Properties and Foreign Keys

--- Vehicle Service Center/VehicleServiceCenter/Models/Appointment.cs
+++ Vehicle Service Center/VehicleServiceCenter/Models/Appointment.cs
@@ -1,15 +1,43 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace VehicleServiceCenter.Models;
 
 public class Appointment
 {
-    
+    [Key]
     public int AppointmentId { get; set; }
+
+    public int CustomerProfileId { get; set; }
+
+    [ForeignKey("CustomerProfile")]
+    public CustomerProfile CustomerProfile { get; set; }
+
+    public int VehicleId { get; set; }
+
+    [ForeignKey("Vehicle")]
+    public Vehicle Vehicle { get; set; }
+
+    public int ServiceTypeId { get; set; }
+
+    [ForeignKey("ServiceType")]
+    public ServiceType ServiceType { get; set; }
+
+    public int MechanicProfileId { get; set; }
+
+    [ForeignKey("MechanicProfile")]
+    public MechanicProfile MechanicProfile { get; set; }
+
+    public int BranchId { get; set; }
+
+    [ForeignKey("Branch")]
+    public Branch Branch { get; set; }
 
     public DateTime AppointmentDate { get; set; }
 
     public string Status { get; set; }
-    // Pending, Confirmed, Completed, Cancelled
 
-    public string Description { get; set; }
+    public string? Notes { get; set; }
 
+    public DateTime CreatedAt { get; set; }
 }
