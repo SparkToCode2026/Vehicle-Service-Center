0a9ae2b5290f30d41781ab78121e087d79f4f744
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-08-06 10:58:40 +0400

    GET All Branches

--- Vehicle Service Center/VehicleServiceCenter/Controllers/BranchController.cs
+++ Vehicle Service Center/VehicleServiceCenter/Controllers/BranchController.cs
@@ -1,6 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace VehicleServiceCenter.Controllers;
 
-public class BranchController
+[ApiController]
+[Route("Branch")]
+public class BranchController : ControllerBase
 {
+    private ProjectContext ProjectContext;
+
+    public BranchController(ProjectContext projectContext)
+    {
+        ProjectContext = projectContext;
+    }
+    
+    // GET All Branches
+    [HttpGet]
+    public async Task<IActionResult> GetAllBranches()
+    {
+        var branches = await context.Branches
+            .ToListAsync();
+
+        return Ok(branches);
+    }
     
 }
