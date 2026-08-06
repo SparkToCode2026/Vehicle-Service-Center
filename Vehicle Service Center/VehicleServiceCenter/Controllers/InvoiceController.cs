c475d0d19900d15f20cdc0460c7b3f77d4782f5d
Marya <maryaalmaskari8@gmail.com>
2026-08-06 11:07:13 +0400

    Add invoice

--- Vehicle Service Center/VehicleServiceCenter/Controllers/InvoiceController.cs
+++ Vehicle Service Center/VehicleServiceCenter/Controllers/InvoiceController.cs
@@ -1,6 +1,32 @@
-﻿namespace VehicleServiceCenter.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using VehicleServiceCenter.Models;
+
+namespace VehicleServiceCenter.Controllers
 {
-    public class InvoiceController
+    [ApiController]
+    [Route("Invoice")]
+    public class InvoiceController : ControllerBase
     {
-    }
-}
+        private ProjectContext ProjectContext;
+
+        public InvoiceController(ProjectContext projectContext)
+        {
+            ProjectContext = projectContext;
+        }
+
+        // Add invoice
+        [HttpPost("AddInvoice")]
+        public IActionResult AddInvoice(InvoiceModel invoice)
+        {
+            // Check whether the service order exists
+            ServiceOrderModel serviceOrder =
+                ProjectContext.ServiceOrders.FirstOrDefault(s =>
+                    s.ServiceOrderId == invoice.ServiceOrderId
+                );
+
+            if (serviceOrder == null)
+            {
+                return BadRequest(
+                    "Service order does not exist"
+                );
+            }
