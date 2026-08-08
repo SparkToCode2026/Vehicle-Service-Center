61dfe1aeb0ec04dd9b375abb708ffd0a59e473f4
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-08-08 12:14:00 +0400

    Update LoginRequest.cs

--- Vehicle Service Center/VehicleServiceCenter/DTOs/LoginRequest.cs
+++ Vehicle Service Center/VehicleServiceCenter/DTOs/LoginRequest.cs
@@ -1,6 +1,14 @@
-﻿namespace VehicleServiceCenter.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VehicleServiceCenter.DTOs
 {
     public class LoginRequest
     {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
     }
 }
