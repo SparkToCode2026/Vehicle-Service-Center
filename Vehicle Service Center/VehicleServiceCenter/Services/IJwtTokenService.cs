c14169485728b490843a5c7b21d3e475e8e575e9
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-08-08 12:15:01 +0400

    Update IJwtTokenService.cs

--- Vehicle Service Center/VehicleServiceCenter/Services/IJwtTokenService.cs
+++ Vehicle Service Center/VehicleServiceCenter/Services/IJwtTokenService.cs
@@ -1,6 +1,10 @@
-﻿namespace VehicleServiceCenter.Services
+﻿using VehicleServiceCenter.DTOs;
+using VehicleServiceCenter.Models;
+
+namespace VehicleServiceCenter.Services
 {
     public class IJwtTokenService
     {
+        LoginResponse CreateToken(UserModel user);
     }
 }
