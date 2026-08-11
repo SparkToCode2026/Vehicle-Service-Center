40494ffeaf59cd702c3b6744fa6a03d787366f25
Mohammed Alsiyabi <alsiyabi1711@gmail.com>
2026-08-11 12:19:33 +0400

    Update api.js

---
+++ Vehicle Service Center/VehicleServiceCenter/frontend/src/api/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL:
+    import.meta.env.VITE_API_BASE_URL || "http://localhost:5000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+// Add the JWT token to every request
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
