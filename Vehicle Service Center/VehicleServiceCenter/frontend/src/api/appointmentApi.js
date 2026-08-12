66e35bf5943b4d60e077f72599c07831f343d7c9
Hawa Alhadi <hawaaalhadi2001@gmail.com>
2026-08-12 15:29:26 +0400

    Get a single appointment by its ID

--- Vehicle Service Center/VehicleServiceCenter/frontend/src/api/appointmentApi.js
+++ Vehicle Service Center/VehicleServiceCenter/frontend/src/api/appointmentApi.js
@@ -1,7 +1,15 @@
 import api from "./api";
 
+// Get all appointments
 export const getAppointments = () => {
     return api
         .get("/Appointment")
+        .then((response) => response.data);
+};
+
+// Get a single appointment by its ID
+export const getAppointmentById = (id) => {
+    return api
+        .get(`/Appointment/${id}`)
         .then((response) => response.data);
 };
